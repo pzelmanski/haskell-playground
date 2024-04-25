2a915502c59c80bbd21fb49c4a1e20a7f6934609
Pawel Zelmanski <pzelmanski1@gmail.com>
2024-04-25 20:11:49 +0200

    factorial

--- factorial/src/Lib.hs
+++ factorial/src/Lib.hs
@@ -8,10 +8,15 @@ factorial :: Integer -> Integer
 factorial 0 = 1
 factorial n = n * factorial (n-1)
 
+factorial' :: Integer -> Integer
+factorial' n 
+    | n == 0 = 1
+    | otherwise = n * factorial (n-1)
 
 someFunc :: IO ()
 someFunc = do
     g <- getStdGen
     let (rand, _) = randomR (1, 20) g
     let v = factorial rand
-    putStrLn ("Factorial of " ++ show rand ++ " is " ++ show v)
+    let v' = factorial' rand
+    putStrLn ("Factorial of " ++ show rand ++ " is " ++ show v ++ " and " ++ show v')
