1f81ab81117584816fb3f51b49913641edad691c
Pawel Zelmanski <pzelmanski1@gmail.com>
2024-05-08 11:03:54 +0200

    game number working

--- advent202302/src/Lib.hs
+++ advent202302/src/Lib.hs
@@ -1,38 +1,38 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (
+    someFunc,
+) where
 
 removeChar :: Char -> String -> String
-removeChar c xs = [x | x <- xs, not (x `elem` [c])] 
+removeChar c xs = [x | x <- xs, not (x `elem` [c])]
 
 getGameNumber :: [String] -> Int
 getGameNumber [] = 0
-getGameNumber game = 
-                read 
-                . removeChar ':'             
-                $ game !! 2 :: Int
+getGameNumber game =
+    read
+        . removeChar ':'
+        $ game !! 2 ::
+        Int
 
-readAndFormat :: String -> IO([[String]])
-readAndFormat filename = do
-                    input <- readFile filename
-                    let result = do
-                            map words
-                            . lines
-                            $ input
-
-                    result
-
-someFunc :: IO ()
-someFunc = do
-        input <- readFile "input1-test.txt"
-        let result = do
+format :: String -> [[String]]
+format input = do
+    let result =
+            do
                 map words
                 . lines
                 $ input
+    result
 
-        -- let gameNumbers = result $ map getGameNumber
 
-        print result
+someFunc :: IO ()
+someFunc = do
+    input <- readFile "input1-test.txt"
+    let formatted = format input
 
-        print input
-        print "Goodbye"
+    print ("first: " ++ show formatted)
+
+    let gameNumbers =  map getGameNumber $ formatted
+    
+    print gameNumbers
+
+    -- print input
+    print "Goodbye"
