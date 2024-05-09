3ea7fc51fe9501573262a1276b1fef3855163e8f
Pawel Zelmanski <pzelmanski1@gmail.com>
2024-05-09 21:12:09 +0200

    maps and binds

--- advent202302/src/Lib.hs
+++ advent202302/src/Lib.hs
@@ -58,20 +58,34 @@ someFunc = do
     input <- readFile "input1-test.txt"
 
     let games = getGames input
-    print ("Games: " ++ show games)
+    -- print ("Games: " ++ show games)
 
     let split =
-            map (splitString ';') 
-            . concatMap (splitString ':') 
-            $ games
-    print (show split)
+            map (splitString ';')
+                . concatMap (splitString ':')
+                $ games
+    -- print (show split)
 
     let split2 = games >>= (splitString ';') >>= (splitString ':')
-    print ("Split2: " ++ show split2)
+    -- print ("Split2: " ++ show split2)
+
+    -- let split3 = map (splitString ';') . ((splitString ':') >>=) $ games
+
+    let bind = games >>= (splitString ':')
+    print ("bind: " ++ show bind)
+
+    let mapp = map (splitString ';') $ bind
+    print ("mapp: " ++ show mapp)
+
+    let bm = map (splitString ';') $ (games >>= (splitString ':'))
+    print ("bm: " ++ show bm)
+
+    let mapmap = map (map (splitString ':')) . map (splitString ';') $ games
+    print ("mapmap: " ++ show mapmap)
 
     let formatted = format input
 
-    print ("first: " ++ show formatted)
+    -- print ("first: " ++ show formatted)
 
     let gameNumbers = map getGameNumber $ formatted
 
