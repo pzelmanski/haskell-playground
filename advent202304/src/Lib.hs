a4a95b31e1c0f2b8a088f5eafa3df48fd8466f9f
Pawel Zelmanski <pzelmanski1@gmail.com>
2024-06-02 21:46:16 +0200

    wip advent 2023 04

--- advent202304/src/Lib.hs
+++ advent202304/src/Lib.hs
@@ -22,13 +22,30 @@ singleGameWins winningNumbers numbers =
         let score = foldl oneOrDouble 0 $ filter (\x -> x `elem` numb) winning
         score
 
+-- this approach wont work for second star, its incorrect
+secondStar :: String -> String -> Int
+secondStar winningNumbers numbers = do
+    let winning = splitString ' ' winningNumbers
+    let numb = splitString ' ' numbers
+    let won = filter (\x -> x `elem` numb) winning
+    length won
+
+ssInput :: [Int] -> [String] -> [String]
+ssInput [] [] = []
+ssInput (c:cs) (x:xs) = (replicate c x) ++ (ssInput cs xs)
+    
+
 advent :: IO ()
 advent = do
-    input <- readFile "input1-prod.txt"
+    input <- readFile "input1-test.txt"
     let lined = lines input
     let ww = map (concat . map (splitString '|') . tail . splitString ':') lined
     -- print ww
 
     let aa = map (\x -> singleGameWins (x !! 0) (x !! 1)) ww
+    let bb = map (\x -> secondStar (x !! 0) (x !! 1)) ww
+    let ss = ssInput bb lined
+    print ("ss input: " ++ show (length ss))
+    print ("second star: " ++ show (bb))
     print ("answer: " ++ show (sum aa))
     print "advtent"
