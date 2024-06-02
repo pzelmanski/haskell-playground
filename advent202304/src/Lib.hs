ebde769d0b505d8904fef71550ba77e53120afb3
Pawel Zelmanski <pzelmanski1@gmail.com>
2024-06-02 21:19:26 +0200

    refactor advent 202304

--- advent202304/src/Lib.hs
+++ advent202304/src/Lib.hs
@@ -9,25 +9,25 @@ import Utils (removeChar, splitString)
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-oneOrDouble :: Int -> Int -> Int
+oneOrDouble :: Int -> String -> Int
 oneOrDouble 0 _ = 1
 oneOrDouble x _ = x * 2
 
 singleGameWins :: String -> String -> Int
-singleGameWins winningNumbers numbers = do
-    let winning = map (\x -> read x :: Int) $ splitString ' ' winningNumbers
-    let numb = map (\x -> read x :: Int) $ splitString ' ' numbers
+singleGameWins winningNumbers numbers =
+    do
+        let winning = splitString ' ' winningNumbers
+        let numb = splitString ' ' numbers
 
-    let common = filter (\x -> x `elem` numb) winning
-    let score = foldl oneOrDouble 0 common
-    score
+        let score = foldl oneOrDouble 0 $ filter (\x -> x `elem` numb) winning
+        score
 
 advent :: IO ()
 advent = do
     input <- readFile "input1-prod.txt"
     let lined = lines input
-    let ww = map (\x -> concat $ map (splitString '|') $ tail $ splitString ':' x) lined
-    print ww
+    let ww = map (concat . map (splitString '|') . tail . splitString ':') lined
+    -- print ww
 
     let aa = map (\x -> singleGameWins (x !! 0) (x !! 1)) ww
     print ("answer: " ++ show (sum aa))
