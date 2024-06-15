b30d4a079a24eee385b5cead4e4988b41040f75b
Pawel Zelmanski <pzelmanski1@gmail.com>
2024-06-16 00:32:57 +0200

    blog 3.3 done

--- blog1/src/Lib.hs
+++ blog1/src/Lib.hs
@@ -1,6 +1,41 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (
+    someFunc,
+) where
+
+import Data.Time.Clock (getCurrentTime)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    time <- getCurrentTime
+    putStrLn
+        ( makeHtml
+            "My page title"
+            $ "My page content;"
+                <> p_ "</br>Timestamp: </br>"
+                <> h1_ (show time)
+        )
+
+makeHtml :: String -> String -> String
+makeHtml title content = html_ $ head_ $ title_ title <> body_ content
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ content = "<body style='background-color:grey'>" <> content <> "</body>"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+el :: String -> String -> String
+el tag content =
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
