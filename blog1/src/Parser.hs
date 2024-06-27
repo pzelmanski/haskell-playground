dd0d78e4ab34a002a8296daa5c6223d4d01bfa95
pawel.zelmanski <pawel.zelmanski@rubrikkgroup.com>
2024-06-27 22:42:42 +0200

    blog 4.2 done

---
+++ blog1/src/Parser.hs
@@ -0,0 +1,24 @@
+module Parser where
+
+import Html.Internal
+
+parse :: String -> Document
+parse = parseLines [] . lines
+
+
+parseLines :: [String] -> [String] -> Document
+parseLines currentParagraph txts = 
+    let
+        paragraph = Paragraph (unlines ( reverse currentParagraph))
+    in
+        case txts of
+            [] -> [paragraph]
+            currentLine : rest -> 
+                if trim currentLine == ""
+                    then 
+                        paragraph : parseLines [] rest
+                    else
+                        parseLines (currentLine : currentParagraph) rest
+
+trim :: String -> String
+trim = unwords . words
